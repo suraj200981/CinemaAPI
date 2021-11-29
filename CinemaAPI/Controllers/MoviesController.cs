ae18067c396950a3f549f9e107c1e312bdfb4fc8
suraj200981 <suraj200981@yahoo.co.uk>
2021-11-29 17:39:16 +0000

    Add project files.

---
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using CinemaAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+
+        private static List<Movie> movies = new List<Movie>
+        {
+            new Movie(){ Id = 0, Name= "Mission Impossible 7", Language="Spanish"},
+             new Movie(){ Id = 1, Name= "James Bond", Language="English"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Movie> Get() {
+
+            return movies;
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody]Movie newMovie) {
+
+            movies.Add(newMovie);
+
+        }
+      
+    }
+}
