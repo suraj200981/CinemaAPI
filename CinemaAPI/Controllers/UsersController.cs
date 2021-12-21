5f73bad78b2a8d42648d67a11ab3ae154abf826b
suraj200981 <suraj200981@yahoo.co.uk>
2021-12-21 18:46:48 +0000

    Implemented login method in users controller using JWT access_token and checking for email and password details in the database. I now need to focus on how the JWT token willl be used.

--- CinemaAPI/Controllers/UsersController.cs
+++ CinemaAPI/Controllers/UsersController.cs
@@ -3,9 +3,12 @@ using CinemaAPI.Data;
 using CinemaAPI.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace CinemaAPI.Controllers
@@ -15,9 +18,13 @@ namespace CinemaAPI.Controllers
     public class UsersController : ControllerBase
     {
         private CinemaDbContext _dbContext;
-        public UsersController(CinemaDbContext dbContext)
+        private IConfiguration _configuration;
+        private readonly AuthService _auth;
+        public UsersController(CinemaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _configuration = configuration;
+            _auth = new AuthService(_configuration);
         }
 
         [HttpPost]
@@ -44,5 +51,41 @@ namespace CinemaAPI.Controllers
 
             return StatusCode(StatusCodes.Status201Created);
         }
+
+
+        [HttpPost]
+        public IActionResult Login([FromBody] User user) {
+
+
+            var userEmail = _dbContext.Users.FirstOrDefault(u => u.Email == user.Email);
+
+            if (userEmail == null) {
+                return NotFound();
+            }
+            //login unsuccessful
+            if (!SecurePasswordHasherHelper.Verify(user.Password, userEmail.Password)) {
+                return Unauthorized();
+            }
+
+            //when login successfull we will generate JWT Token
+            var claims = new[]
+                {
+                   new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                   new Claim(ClaimTypes.Email, user.Email),
+                };
+            var token = _auth.GenerateAccessToken(claims);
+            return new ObjectResult(new
+            {
+                access_token = token.AccessToken,
+                expires_in = token.ExpiresIn,
+                token_type = token.TokenType,
+                creation_Time = token.ValidFrom,
+                expiration_Time = token.ValidTo,
+                user_id = userEmail.Id
+            });
+        }
+
+
+
     }
 }
--- CinemaAPI/Startup.cs
+++ CinemaAPI/Startup.cs
@@ -12,6 +12,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 
 namespace CinemaAPI
 {
@@ -29,6 +32,21 @@ namespace CinemaAPI
         {
             services.AddControllers();
             services.AddDbContext<CinemaDbContext>(options => options.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB;Database=CinemaDb; Integrated Security = True"));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuer = true,
+                      ValidateAudience = true,
+                      ValidateLifetime = true,
+                      ValidateIssuerSigningKey = true,
+                      ValidIssuer = Configuration["Tokens:Issuer"],
+                      ValidAudience = Configuration["Tokens:Issuer"],
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"])),
+                      ClockSkew = TimeSpan.Zero,
+                  };
+              });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +63,9 @@ namespace CinemaAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            //authentication always comes before authorization
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
