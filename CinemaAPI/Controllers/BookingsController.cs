2c2c97c8d6100caa90c8030ca941ce71a0c665d6
suraj200981 <suraj200981@yahoo.co.uk>
2021-12-24 21:07:12 +0000

    Authorised users can create a booking

---
+++ CinemaAPI/Controllers/BookingsController.cs
@@ -0,0 +1,47 @@
+﻿using CinemaAPI.Data;
+using CinemaAPI.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+
+        public BookingsController(CinemaDbContext dbContext)
+        {
+            _dbContext  = dbContext;
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult Get() {
+
+            return Ok(_dbContext.Bookings);
+        
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Post([FromForm] Booking booking) {
+
+            //gets the exact date and time of booking so it does not need to be passed in the form
+            booking.ReservationTime = DateTime.Now;
+
+            _dbContext.Bookings.Add(booking);
+            _dbContext.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
--- CinemaAPI/Entities/Booking.cs
+++ CinemaAPI/Entities/Booking.cs
@@ -10,6 +10,7 @@ namespace CinemaAPI.Entities
         public int Id { get; set; }
         public int Qty { get; set; }
         public double Price { get; set; }
+        public string Phone { get; set; }
         public DateTime ReservationTime { get; set; }
         public int MovieId { get; set; }
         public int UserId { get; set; }
